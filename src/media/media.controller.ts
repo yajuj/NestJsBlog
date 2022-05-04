413b0075e1b4d9d9b6e1eac9afcd38b4746a7f1b
yajuj <noticethatred@gmail.com>
2022-05-05 04:38:51 +0500

    port

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,10 +1,47 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
-import { Response } from 'express';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  Res,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { Request, Response } from 'express';
 import { createReadStream } from 'fs';
 import { join } from 'path';
+import { saveMediaToStorage } from 'src/utils/storage';
 
 @Controller('media')
 export class MediaController {
+  // @Post()
+  // @UseInterceptors(
+  //   FileFieldsInterceptor(
+  //     [
+  //       { name: 'photo', maxCount: 1 },
+  //       { name: 'video', maxCount: 1 },
+  //     ],
+  //     saveMediaToStorage,
+  //   ),
+  // )
+  // create(
+  //   @Req() req: Request,
+  //   @UploadedFiles()
+  //   files: { photo?: Express.Multer.File[]; video?: Express.Multer.File[] },
+  // ) {
+  //   const { hostname } = req;
+  //   const photo = files.photo
+  //     ? `http://${hostname}/media/${files?.photo[0]?.filename}`
+  //     : undefined;
+  //   const video = files.video
+  //     ? `http://${hostname}/media/${files?.video[0]?.filename}`
+  //     : undefined;
+
+  //   return { photo, video: video ? video : '' };
+  // }
+
   @Get(':mediaId')
   find(@Param('mediaId') mediaId: string, @Res() res: Response) {
     return createReadStream(join(process.cwd(), `media/${mediaId}`)).pipe(res);
